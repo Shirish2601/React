963a238bc968e9fb9c975731202075bbb3bb45f1
shirish2601 <you@example.com>
2023-04-08 14:36:40 +0530

    changed login page

--- src/components/UserLogin/UserLogin.jsx
+++ src/components/UserLogin/UserLogin.jsx
@@ -3,16 +3,19 @@ import "./UserLogin.css";
 import { Link } from "react-router-dom";
 const UserLogin = (props) => {
   return (
-    <form className="login">
-      <h2>Welcome, {props.userRole === "student" ? "Student" : "Admin"}!</h2>
-      <p>Please log in</p>
-      <input type="text" placeholder="PRN" />
-      <input type="password" placeholder="Password" />
-      <input type="submit" value="Log In" />
-      <div className="links">
-        <Link to="/">Forgot password</Link>
-      </div>
-    </form>
+    <div className="wrapper">
+      <img src="..\src\assets\LoginImageArt.svg" alt="Login Image" />
+      <form className="login">
+        <h2>Welcome, {props.userRole === "student" ? "Student" : "Admin"}!</h2>
+        <p>Please log in</p>
+        <input type="text" placeholder="PRN" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Log in</button>
+        <div className="links">
+          <Link to="/">Forgot password ?</Link>
+        </div>
+      </form>
+    </div>
   );
 };
 
