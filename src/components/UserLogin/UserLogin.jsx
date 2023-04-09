3d32baa8aba8f411e3b4fea779cded59ad0302ea
shirish2601 <you@example.com>
2023-04-09 15:43:33 +0530

    added login

--- src/components/UserLogin/UserLogin.jsx
+++ src/components/UserLogin/UserLogin.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+
 import "./UserLogin.css";
-import { Link } from "react-router-dom";
+
 const UserLogin = (props) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [authorized, setAuthorized] = useState(false);
+
+  const buttonSubmitHandler = (e) => {
+    e.preventDefault();
+    const userData = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      authorized: false,
+    };
+    if (
+      emailRef.current.value === "abc" &&
+      passwordRef.current.value === "abc"
+    ) {
+      userData.authorized = true;
+    } else {
+      userData.authorized = false;
+    }
+    setAuthorized(userData.authorized);
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+  };
+
   return (
     <div className="wrapper">
       <img src="..\src\assets\LoginImageArt.svg" alt="Login Image" />
@@ -11,9 +37,18 @@ const UserLogin = (props) => {
         <input
           type="text"
           placeholder={props.userRole === "student" ? "PRN" : "Email"}
+          ref={emailRef}
         />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Log in</button>
+        <input type="password" placeholder="Password" ref={passwordRef} />
+        <button
+          className="user-login__button"
+          type="submit"
+          onClick={buttonSubmitHandler}
+        >
+          Log in
+        </button>
+        {authorized && <Redirect to="/student/" />}
+        {!authorized && <Redirect to="/login/student/" />}
         <div className="links">
           <Link to="/">Forgot password ?</Link>
         </div>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import UserLogin from "../components/UserLogin/UserLogin";
 const LoginPage = (props) => {
-  return <UserLogin userRole={props.user} />;
+  return <UserLogin userRole={props.user}></UserLogin>;
 };
 export default LoginPage;
